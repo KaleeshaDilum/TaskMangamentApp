85ea98e9fef86867e56eff58dc6e772ff1d483e8
KaleeshaDilum <kaleeshadilum22@gmail.com>
2024-05-06 23:51:19 +0530

    XML UI activity main + activity add note

--- app/src/main/java/com/example/taskmangamentapp/AddNoteActivity.kt
+++ app/src/main/java/com/example/taskmangamentapp/AddNoteActivity.kt
@@ -6,12 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class AddNoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.addButton)) { v, insets ->
+        setContentView(R.layout.activity_add_note)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.addNoteHeading)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
