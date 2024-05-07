1ec08abd439acd1115020d261d36de8855234fc2
KaleeshaDilum <kaleeshadilum22@gmail.com>
2024-05-07 21:47:42 +0530

    create SQL DB and add Notes

--- app/src/main/java/com/example/taskmangamentapp/AddNoteActivity.kt
+++ app/src/main/java/com/example/taskmangamentapp/AddNoteActivity.kt
@@ -1,20 +1,33 @@
 package com.example.taskmangamentapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.taskmangamentapp.databinding.ActivityAddNoteBinding
+import com.example.taskmangamentapp.databinding.ActivityMainBinding
 
 class AddNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var db:NoteDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_add_note)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.addNoteHeading)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding=ActivityAddNoteBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        db= NoteDatabase(this)
+        binding.saveButton.setOnClickListener{
+          val title=binding.titleEditText.text.toString() // get user inputs title
+            val content =binding.contentEditText.text.toString()// get user input content
+            val note =Note (0,title,content)// Arguments pass in data class
+            db.insertNote(note)// calling insert note function
+            finish() // finish use to close the activity and riderct to main
+            Toast.makeText(this,"Note saved successfully",Toast.LENGTH_LONG).show()
         }
+
+
     }
 }
